8c76de3cbcf783bdccf95b390a9090b11924a5cd
skytravel <dev4st@gmail.com>
2012-07-06 01:29:53 +0900

    板ポリゴン描画用のプロジェクトを追加

---
+++ Source/08_main.cpp
@@ -0,0 +1,53 @@
+﻿/**
+ * Referenced :
+ * http://www21.atwiki.jp/opengl/pages/24.html
+ */
+#pragma comment(linker, "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
+
+#include <cstdio>
+#include <cstdint>
+#include <freegl/glut.h>
+
+#include "TestLib.h"
+
+using namespace std;
+
+namespace {
+	const int DISPLAY_WIDTH = 320;
+	const int DISPLAY_HEIGHT = 240;
+}// unnamed namespace
+
+void initialize() {
+	glClearColor( 1.0f, 1.0f, 1.0f, 1.0f );
+	glOrtho( 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, -1, 1 );
+}
+
+void terminate() {
+}
+
+void display() {
+	glClear( GL_COLOR_BUFFER_BIT );
+	
+
+	glutSwapBuffers();
+}
+
+void idle() {
+	glutPostRedisplay();
+}
+
+int main( int argc, char *argv[] ) {
+	glutInitWindowPosition( 100, 100 );
+	glutInitWindowSize( DISPLAY_WIDTH, DISPLAY_HEIGHT );
+	glutInit( &argc, argv );
+	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+	glutCreateWindow( "Hello OpenGL!!" );
+	glutDisplayFunc( display );
+	glutIdleFunc( idle );
+
+	initialize();
+	glutMainLoop();
+	terminate();
+	return 0;
+}
+
