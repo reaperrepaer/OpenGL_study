cf78adda7819fb376ac464fc5fccfbf54c05efe3
skytravel <dev4st@gmail.com>
2012-07-05 18:40:07 +0900

    01の中身を記述、処理追加、コメント追加。

--- Source/00_main.cpp
+++ Source/00_main.cpp
@@ -2,12 +2,24 @@
 #include <freegl/glut.h>
 
 void display() {
+	// カラーバッファのクリア色を指定
+	glClearColor( 0.0, 0.0, 1.0, 1.0 );
+	// カラーバッファをクリアする
+	glClear( GL_COLOR_BUFFER_BIT );
+	// ここまでの描画コマンドを実行
+	glFlush();
 }
 
 int main( int argc, char *argv[] ) {
+	// GLUTを初期化
 	glutInit( &argc, argv );
+	// ウィンドウを生成
 	glutCreateWindow( "Hello OpenGL!!" );
+	// ディスプレイモードの設定。指定しなくてもいいが、GLUT_RGBAがデフォルトになる。
+	glutInitDisplayMode( GLUT_RGBA );
+	// ディスプレイコールバック関数の登録
 	glutDisplayFunc( display );
+	// イベント処理を開始
 	glutMainLoop();
 	return 0;
 }
--- Source/01_main.cpp
+++ Source/01_main.cpp
@@ -2,9 +2,17 @@
 #include <freegl/glut.h>
 
 void display() {
+	glClearColor( 0.0, 0.0, 1.0, 1.0 );
+	glClear( GL_COLOR_BUFFER_BIT );
+	glFlush();
 }
 
 int main( int argc, char *argv[] ) {
+	// ウィンドウの初期位置を指定
+	glutInitWindowPosition( 100, 100 );
+	// ウィンドウのサイズを指定
+	glutInitWindowSize( 320, 240 );
+
 	glutInit( &argc, argv );
 	glutCreateWindow( "Hello OpenGL!!" );
 	glutDisplayFunc( display );
