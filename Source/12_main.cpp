37ee0a41b5ed1b4eb2cc57225d6163acfc461ff4
skytravel <dev4st@gmail.com>
2012-07-06 22:05:50 +0900

    URLを修正

--- Source/12_main.cpp
+++ Source/12_main.cpp
@@ -1,6 +1,6 @@
 ﻿/**
  * Referenced :
- * http://www21.atwiki.jp/opengl/pages/29.html
+ * http://www21.atwiki.jp/opengl/pages/99.html
  */
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
 
