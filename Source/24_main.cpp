0081072e2c5618148d66d56e7c5c1e79833c620c
skytravel <dev4st@gmail.com>
2012-07-12 08:46:28 +0900

    lodepng用のプロジェクトを追加。テクスチャマトリクス用のプロジェクトを追加

--- Source/24_main.cpp
+++ Source/24_main.cpp
@@ -1,18 +1,13 @@
 ﻿/**
  * Referenced :
- * http://www21.atwiki.jp/opengl/pages/57.html
- * boost::filesystem
- * http://hwada.hatenablog.com/entry/20110611/1307781684
- * http://d.hatena.ne.jp/osyo-manga/20100924/1285277564
+ * http://www21.atwiki.jp/opengl/pages/46.html
  */
 #pragma comment(linker, "/SUBSYSTEM:WINDOWS /ENTRY:mainCRTStartup")
 
 #include <cstdio>
 #include <cstdint>
 #include <freegl/glut.h>
-// glpngを使う
 #include <gl/glpng.h>
-// boost::filesystemを使う
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/operations.hpp>
 
@@ -25,6 +20,7 @@ namespace {
 	const int DISPLAY_HEIGHT = 240;
 
 	GLfloat lightpos[] = { 200.0f, 1000.0f, -500.0f, 1.0f };
+	float angleZ = 0.0f;
 
 	pngInfo info;
 	GLuint texture;
@@ -33,20 +29,12 @@ namespace {
 
 void initialize() {
 	glClearColor( 0.3f, 0.3f, 0.3f, 1.0f );
-/*
-	glEnable( GL_DEPTH_TEST );
-	glEnable( GL_LIGHTING );
-	glEnable( GL_LIGHT0 );
-*/
 	glOrtho( 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, -1, 1 );
 	
-	// カレントディレクトリ取得
 	namespace fs = boost::filesystem;  
-	fs::path image_path = fs::current_path() / "data/image.png";
-
+	fs::path image_path = fs::current_path() / "data/mapchip.png";
 	std::string str = image_path.string();
 
-	// テクスチャを作る
 	texture = pngBind (
 				str.c_str(),
 				PNG_NOMIPMAP,
@@ -66,18 +54,22 @@ void display() {
 
 	glMatrixMode( GL_MODELVIEW );
 	glLoadIdentity();
-
-	// 久しぶりに正射影
 	glOrtho( 0.0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0.0, -1.0, 1.0 );
 
-	glEnable( GL_TEXTURE_2D );// テクスチャ有効
-	glEnable( GL_ALPHA_TEST );// アルファテスト開始
+	// 行列をテクスチャモードに変更
+	// テクスチャも射影行列とかのように移動拡大回転などができる。
+	glMatrixMode( GL_TEXTURE );
+	// 単位行列を設定
+	glLoadIdentity();
+	// Z軸回転
+	glRotatef( angleZ, 0.0f, 0.0f, 1.0f );// Z軸を回転
+
+	glEnable( GL_TEXTURE_2D );
+	glEnable( GL_ALPHA_TEST );
 
 	glBegin( GL_POLYGON );
 
 	// 頂点座標とテクスチャ座標の設定
-	// これは順番に入れないといけないらしい。
-	// この辺は順番に追加されていくんだろうと思う。
 	// 左下
 	glTexCoord2f( 0.0f, 1.0f );
 	glVertex2d(  10, 230 );
@@ -93,13 +85,14 @@ void display() {
 
 	glEnd();
 
-	glDisable( GL_ALPHA_TEST );// アルファテスト終了
-	glDisable( GL_TEXTURE_2D );// テクスチャ無効
+	glDisable( GL_ALPHA_TEST );
+	glDisable( GL_TEXTURE_2D );
 
 	glutSwapBuffers();
 }
 
 void idle() {
+	angleZ += 0.02f;
 	glutPostRedisplay();
 }
 
