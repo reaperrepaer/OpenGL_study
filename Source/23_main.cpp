2f754998480882d05b6c4fe90d3313fb93ae0386
skytravel <dev4st@gmail.com>
2012-07-11 18:16:57 +0900

    png読み込みテスト用の画像を追加。あとプロジェクトファイル修正
    boostのsystem.libとfilesystem.libを追加。

--- Source/23_main.cpp
+++ Source/23_main.cpp
@@ -12,6 +12,7 @@
 
 #include "TestLib.h"
 
+#if 0
 using namespace std;
 
 namespace {
@@ -35,7 +36,7 @@ void initialize() {
 	
 	// テクスチャを作る
 	texture = pngBind (
-				"sample.png",
+				"image.png",
 				PNG_NOMIPMAP,
 				PNG_ALPHA,
 				&info,
@@ -100,4 +101,61 @@ int main( int argc, char *argv[] ) {
 	terminate();
 	return 0;
 }
+#endif
 
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+
+#define WIDTH 320
+#define HEIGHT 240
+
+pngInfo info;
+GLuint texture;
+
+void display(void)
+{
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+ glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+ glOrtho(0.0, WIDTH, HEIGHT, 0.0, -1.0, 1.0);
+
+ glEnable(GL_TEXTURE_2D);//テクスチャ有効
+    glEnable(GL_ALPHA_TEST);//アルファテスト開始
+    glBegin(GL_POLYGON);
+ glTexCoord2f(0.0f, 1.0f); glVertex2d(10 , 230);//左下
+ glTexCoord2f(0.0f, 0.0f); glVertex2d(10 ,  10);//左上
+ glTexCoord2f(1.0f, 0.0f); glVertex2d( 310 ,  10);//右上
+ glTexCoord2f(1.0f, 1.0f); glVertex2d( 310 , 230);//右下
+    glEnd();
+    glDisable(GL_ALPHA_TEST);//アルファテスト終了
+    glDisable(GL_TEXTURE_2D);//テクスチャ無効
+
+ glutSwapBuffers();
+}
+void idle(void)
+{
+ glutPostRedisplay();
+}
+void Init(){
+ glClearColor(0.0, 0.0, 0.0, 1.0);
+ glOrtho(0, WIDTH, HEIGHT, 0, -1, 1);
+
+ // カレントディレクトリ取得
+ namespace fs = boost::filesystem;  
+ fs::path image_path = fs::current_path() / "data/image.png";
+
+ texture = pngBind("C:/st@workspace/GitHub/OpenGL_study/app/image.png", PNG_NOMIPMAP, PNG_ALPHA, &info, GL_CLAMP, GL_NEAREST, GL_NEAREST);
+}
+int main(int argc, char *argv[])
+{
+ glutInitWindowPosition(100, 100);
+ glutInitWindowSize(WIDTH, HEIGHT);
+ glutInit(&argc, argv);
+ glutCreateWindow("画像を読み込んで表示");
+ glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+ glutDisplayFunc(display);
+ glutIdleFunc(idle);
+ Init();
+ glutMainLoop();
+ return 0;
+}
