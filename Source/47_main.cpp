51c136e674525b97b648dd98d15acfd0c0c5dc46
skytravel <dev4st@gmail.com>
2012-08-19 21:23:19 +0900

    基本図形の描画を追加

--- Source/47_main.cpp
+++ Source/47_main.cpp
@@ -56,6 +56,10 @@ namespace {
 	const int DISPLAY_WIDTH = 320;
 	const int DISPLAY_HEIGHT = 240;
 
+	const float PAI = 3.1415926535897932384626433832795f;
+	const float TO_RAD = PAI / 180.0f;
+	const float TO_DEG = 180.0f / PAI;
+
 	// ライトの位置
 	GLfloat lightPos[] = { 200.0f, 150.0f, -500.0f, 1.0f };
 	// 赤
@@ -66,6 +70,39 @@ namespace {
 	GLuint texID;
 	Texture* texture;
 
+	// 3D描画用の設定
+	void setup3D() {
+		// 2D用に押し込んでいたマトリクスを戻す(親階層)
+		glMatrixMode( GL_PROJECTION );
+		glPopMatrix();
+		glMatrixMode( GL_MODELVIEW );
+		glPopMatrix();					
+
+		glEnable( GL_DEPTH_TEST );
+		glEnable( GL_LIGHTING );
+		glEnable( GL_LIGHT0 );
+	}
+
+	// 2D描画用の設定
+	void setup2D() {
+		// 押し込んで正射影を設定(子階層)
+		glMatrixMode( GL_PROJECTION );
+		glPushMatrix();
+		glLoadIdentity();
+		glOrtho( 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, -1, 1 );
+		glMatrixMode( GL_MODELVIEW );
+		glPushMatrix();
+		glLoadIdentity();
+	
+		glDisable( GL_DEPTH_TEST );
+		glDisable( GL_LIGHTING );
+		glDisable( GL_LIGHT0 );
+	}
+
+	void setColor( float r, float g, float b, float a ) {
+		glColor4f( r, g, b, a );
+	}
+
 	// ストレッチ描画
 	void drawStretch( Rect dst, Rect src ) {
 		glEnable( GL_TEXTURE_RECTANGLE_EXT );// 拡張機能を使う
@@ -74,21 +111,133 @@ namespace {
 		glEnable( GL_ALPHA_TEST );// アルファテスト開始
 		glBegin( GL_POLYGON );
 		// 左下
+		glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
 		glTexCoord2i( src.x, src.y+src.height );
 		glVertex2d( dst.x, dst.y+dst.height );
 		// 左上
+		glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
 		glTexCoord2i( src.x, src.y );
 		glVertex2d( dst.x, dst.y );
 		// 右上
+		glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
 		glTexCoord2i( src.x+src.width, src.y );
 		glVertex2d( dst.x+dst.width, dst.y );
 		// 右下
+		glColor4f( 1.0f, 1.0f, 1.0f, 1.0f );
 		glTexCoord2i( src.x+src.width, src.y+src.height );
 		glVertex2d( dst.x+dst.width, dst.y+dst.height );
 
 		glEnd();
 		glDisable( GL_ALPHA_TEST );// アルファテスト終了
 		glDisable( GL_TEXTURE_RECTANGLE_EXT );
+	}
+
+	void drawPoint( int x, int y, float size ) {
+		glPointSize( size );
+		glBegin( GL_POINTS );
+		glVertex2i( x, y );
+		glEnd();
+	}
+
+	void drawLine( int x, int y, int x2, int y2, float lineWidth ) {
+		glLineWidth( lineWidth );
+		glBegin( GL_LINES );
+		glVertex2i( x, y );
+		glVertex2i( x2, y2 );
+		glEnd();
+	}
+
+	void drawRect( int x, int y, int w, int h, float lineWidth ) {
+		glLineWidth( lineWidth );
+		glBegin( GL_LINE_LOOP );
+		glVertex2i( x, y );
+		glVertex2i( x+w, y );
+		glVertex2i( x+w, y+h );
+		glVertex2i( x, y+h );
+		glEnd();
+	}
+
+	void drawFillRect( int x, int y, int w, int h ) {
+		glBegin( GL_QUADS );
+		glVertex2i( x, y );
+		glVertex2i( x+w, y );
+		glVertex2i( x+w, y+h );
+		glVertex2i( x, y+h );
+		glEnd();
+	}
+
+	void drawCircle( float radius, int x, int y ) {
+		for ( float th1 = 0.0f;  th1 <= 360.0f;  th1 = th1 + 1.0f ) {             
+			float th2 = th1 + 10.0f;
+			float th1_rad = th1 * TO_RAD; 
+			float th2_rad = th2 * TO_RAD;
+
+			float x1 = radius * cos( th1_rad );
+			float y1 = radius * sin( th1_rad );
+			float x2 = radius * cos( th2_rad );
+			float y2 = radius * sin( th2_rad );
+
+			glBegin( GL_LINES );
+			glVertex2f( x1+x, y1+y );     
+			glVertex2f( x2+x, y2+y );
+			glEnd();
+		}
+	}
+
+	void drawCircleFill( float radius, int x, int y ) {
+		for ( float th1 = 0.0f;  th1 <= 360.0f;  th1 = th1 + 1.0f ) {
+			float th2 = th1 + 10.0f;
+			float th1_rad = th1 * TO_RAD; 
+			float th2_rad = th2 * TO_RAD;
+
+			float x1 = radius * cos( th1_rad );
+			float y1 = radius * sin( th1_rad );
+			float x2 = radius * cos( th2_rad );
+			float y2 = radius * sin( th2_rad );
+
+			glBegin( GL_TRIANGLES );
+			glVertex2f( static_cast<float>(x), static_cast<float>(y) );
+			glVertex2f( x1+x, y1+y );     
+			glVertex2f( x2+x, y2+y );
+			glEnd();
+		} 
+	}
+
+	void drawOval( float radius,int x, int y, float ovalx, float ovaly ) {
+		for ( float th1 = 0.0f;  th1 <= 360.0f;  th1 = th1 + 1.0f ) {
+			float th2 = th1 + 10.0f;
+			float th1_rad = th1 * TO_RAD; 
+			float th2_rad = th2 * TO_RAD;
+
+			float x1 = radius * cos( th1_rad ) * (ovalx / 100.0f);
+			float y1 = radius * sin( th1_rad ) * (ovaly / 100.0f);
+			float x2 = radius * cos( th2_rad ) * (ovalx / 100.0f);
+			float y2 = radius * sin( th2_rad ) * (ovaly / 100.0f);
+
+			glBegin( GL_LINES );
+			glVertex2f( x1+x, y1+y );     
+			glVertex2f( x2+x, y2+y );
+			glEnd();
+		}
+	}
+
+	void drawOvalFill( float radius, int x, int y, float ovalx, float ovaly ) {
+		for ( float th1 = 0.0f;  th1 <= 360.0f; th1 = th1 + 1.0f ) {
+			float th2 = th1 + 10.0f;
+			float th1_rad = th1 * TO_RAD; 
+			float th2_rad = th2 * TO_RAD;
+
+			float x1 = radius * cos( th1_rad ) * (ovalx / 100.0f);
+			float y1 = radius * sin( th1_rad ) * (ovaly / 100.0f);
+			float x2 = radius * cos( th2_rad ) * (ovalx / 100.0f);
+			float y2 = radius * sin( th2_rad ) * (ovaly / 100.0f);
+
+			glBegin( GL_TRIANGLES );
+			glVertex2f( static_cast<float>(x), static_cast<float>(y) );
+			glVertex2f( x1+x, y1+y );
+			glVertex2f( x2+x, y2+y );
+			glEnd();
+		}
 	}
 
 }// unnamed namespace
@@ -118,33 +267,6 @@ void initialize() {
 void finalize() {
 }
 
-void setup3D() {
-	// 2D用に押し込んでいたマトリクスを戻す(親階層)
-	glMatrixMode( GL_PROJECTION );
-	glPopMatrix();
-	glMatrixMode( GL_MODELVIEW );
-	glPopMatrix();					
-
-	glEnable( GL_DEPTH_TEST );
-	glEnable( GL_LIGHTING );
-	glEnable( GL_LIGHT0 );
-}
-
-void setup2D() {
-	// 押し込んで正射影を設定(子階層)
-	glMatrixMode( GL_PROJECTION );
-	glPushMatrix();
-	glLoadIdentity();
-	glOrtho( 0, DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, -1, 1 );
-	glMatrixMode( GL_MODELVIEW );
-	glPushMatrix();
-	glLoadIdentity();
-	
-	glDisable( GL_DEPTH_TEST );
-	glDisable( GL_LIGHTING );
-	glDisable( GL_LIGHT0 );
-}
-
 void update() {
 }
 
@@ -172,34 +294,45 @@ void draw() {
 	// マテリアルの設定
 	glMaterialfv( GL_FRONT, GL_DIFFUSE, red );
 
-	glRotatef( angleX, 1.0f, 0.0f, 0.0f );
-
 	// キューブを描画
+	glRotatef( angleX, 1.0f, 0.0f, 0.0f );
 	glutSolidCube( 40.0f );
 	// ------------------------------------
 
 	setup2D();
 	// ----------- ここに2D描画 -----------
-	/*
-	glBegin( GL_QUADS );
-	glColor4f( 1.0, 1.0, 0.0, 1.0f );
-	glVertex3f( 100 , 180, 0 );
-	glColor4f( 1.0, 1.0, 1.0, 1.0f );
-	glVertex3f( 200 , 180, 0 );
-	glColor4f( 0.0, 1.0, 1.0, 1.0f );
-	glVertex3f( 200 , 220, 0 );
-	glColor4f( 0.0, 0.0, 0.0, 1.0f );
-	glVertex3f( 100 , 220, 0 );
-	glEnd();
-	*/
-
 	glEnable( GL_TEXTURE_2D );
 
 	Rect dst( 0, 0, 128, 128 );// 貼り付ける元になる矩形
 	Rect src( 0, 0, 64, 64 );	// テクスチャのどの部分を貼り付けるか
+	setColor( 1.0f, 1.0f, 1.0f, 1.0f );
 	drawStretch( dst, src );
 
 	glDisable( GL_TEXTURE_2D );
+
+	setColor( 1.0f, 0.0f, 0.0f, 1.0f );
+	drawPoint( 150, 150, 5.0f );
+
+	setColor( 0.0f, 1.0f, 0.0f, 1.0f );
+	drawLine( 0, DISPLAY_HEIGHT, DISPLAY_WIDTH, 0, 3.0f );
+
+	setColor( 0.0f, 0.0f, 1.0f, 1.0f );
+	drawRect( 220, 10, 30, 30, 2.0f );
+
+	setColor( 1.0f, 1.0f, 0.0f, 1.0f );
+	drawFillRect( 220, 100, 30, 30 );
+
+	setColor( 1.0f, 0.0f, 1.0f, 1.0f );
+	drawCircle( 40.0f, 50, 50 );
+
+	setColor( 0.0f, 1.0f, 1.0f, 1.0f );
+	drawCircleFill( 50.0f, 150, 180 );
+
+	setColor( 0.5f, 0.5f, 1.0f, 1.0f );
+	drawOval( 50.0f, 150, 80, 100.0f, 80.0f );
+
+	setColor( 1.0f, 1.0f, 1.0f, 1.0f );
+	drawOvalFill( 80.0f, 260, 120, 50.0f, 100.0f );
 
 	// ------------------------------------
 
